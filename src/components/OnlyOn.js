b4e19edadf1b1eb361a0d150341645e0af228621
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-07-24 21:10:49 +0500

    Bug Fixes

--- src/components/OnlyOn.js
+++ src/components/OnlyOn.js
@@ -6,7 +6,7 @@ import logo2 from "../assets/img/favicon.ico";
 export default function OnlyOn() {
   return (
     <div>
-      <navbar className="navbar">
+      <navbar className="navbar-2">
         <div className="nav-logo">
           <img src={logo} alt="Netflix" id="logo1" width={100} />
           <img src={logo2} alt="Netflix" id="logo2" width={30} />
