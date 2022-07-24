7fe1b0b017694f15ba4d17e1c105f378586bd0fc
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-07-24 19:20:13 +0500

    Home Completed

--- src/components/Section6.js
+++ src/components/Section6.js
@@ -2,8 +2,68 @@ import React from "react";
 
 export default function Section6() {
   return (
-    <div className="section section-6">
-      <div className="section-inner"></div>
+    <div className="section footer section-6">
+      <div className="section-inner footer-inner">
+        <div className="footer-sec-1">
+          <div className="footer-contact">
+            <a href="./contact">Questions? Contact Us</a>
+          </div>
+          <div className="footer-faq">
+            <a href="./faq">FAQ</a>
+          </div>
+          <div className="footer-investor">
+            <a href="./contact">Investor Relations</a>
+          </div>
+          <div className="footer-privacy">
+            <a href="./contact">Privacy</a>
+          </div>
+          <div className="footer-speed">
+            <a href="./contact">Speed Test</a>
+          </div>
+          <div className="footer-location">
+            <a href="./contact">Netflix Pakistan</a>
+          </div>
+        </div>
+        <div className="footer-sec-1">
+          <div className="footer-faq">
+            <a href="./faq">Help Center</a>
+          </div>
+          <div className="footer-investor">
+            <a href="./contact">Jobs</a>
+          </div>
+          <div className="footer-privacy">
+            <a href="./contact">Cookie Preference</a>
+          </div>
+          <div className="footer-speed">
+            <a href="./contact">Legal Notices</a>
+          </div>
+        </div>
+        <div className="footer-sec-1  footer-seclast-sec">
+          <div className="footer-faq">
+            <a href="./faq">Account</a>
+          </div>
+          <div className="footer-privacy">
+            <a href="./contact">Privacy</a>
+          </div>
+          <div className="footer-speed">
+            <a href="./contact">Corporate Information</a>
+          </div>
+          <div className="footer-investor">
+            <a href="./contact">Only On Netflix</a>
+          </div>
+        </div>
+        <div className="footer-sec-1 footer-last-sec">
+          <div className="footer-faq">
+            <a href="./faq">Media Center</a>
+          </div>
+          <div className="footer-investor">
+            <a href="./contact">Terms Of Use</a>
+          </div>
+          <div className="footer-privacy">
+            <a href="./contact">Contact Us</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
