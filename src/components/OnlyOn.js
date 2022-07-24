fe68aafbd97f71e8a655dd01543dfee6d37916cf
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-07-24 21:09:03 +0500

    Only On Component Navbar Added

--- src/components/OnlyOn.js
+++ src/components/OnlyOn.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import logo from "../assets/img/logo-2.png";
+import logo2 from "../assets/img/favicon.ico";
 
 export default function OnlyOn() {
   return (
     <div>
       <navbar className="navbar">
         <div className="nav-logo">
-          <img src={logo} alt="Netflix" width={100} />
+          <img src={logo} alt="Netflix" id="logo1" width={100} />
+          <img src={logo2} alt="Netflix" id="logo2" width={30} />
         </div>
         <div className="nav-right">
           <p>Unlimited TV Shows & Movies</p>
