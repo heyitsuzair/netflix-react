0c07ef039f674d659d598c90bc04635c5e9e85e0
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-07-24 19:24:52 +0500

    Home Completed

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
 import Section5 from "./components/Section5";
-import LoadingBar from "react-top-loading-bar";
-import { useState } from "react";
+// import LoadingBar from "react-top-loading-bar";
+// import { useState } from "react";
 import Section6 from "./components/Section6";
 
 function App() {
-  const [progress, setProgress] = useState(0);
-  const setProgressTo = (setProgressTo) => {
-    setProgress(setProgressTo);
-  };
+  // const [progress, setProgress] = useState(0);
+  // const setProgressTo = (setProgressTo) => {
+  //   setProgress(setProgressTo);
+  // };
 
   return (
     <div className="App">
-      <LoadingBar olor="#f11946" progress={progress} height={3} />
+      {/* <LoadingBar olor="#f11946" progress={progress} height={3} /> */}
       <Hero />
       <Section1 />
       <Section2 />
